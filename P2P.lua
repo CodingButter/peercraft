2046ac400f8794a4f47dc7178439d6d2223c4aac
Jamie Nichols <Jamie337nichols@gmail.com>
2022-01-26 23:30:32 -0500

    Updated p2p

--- P2P.lua
+++ P2P.lua
@@ -1,20 +1,36 @@
 local json = require "json"
 local class = require "class"
-local switch = require "switch"
 local PEER_LOBBY = "http://beastcraft.codingbutter.com:8888/"
 local WS_SERVER = "ws://beastcraft.codingbutter.com:3028"
 local P2P = class({
     dispatch = {
         connected = function()
-            print("connected")
         end
     },
     constructor = function(self, computerusername)
         self.username = computerusername or os.getComputerID()
+        self.peers = {}
         self.ws, self.err = http.websocket(WS_SERVER)
         if self.ws then
             self.connected = true
         end
+    end,
+    attachEventLoop = function(self, eventLoop)
+        parallel.waitForAll(function()
+            while self.connected do
+                local msg = self.ws.receive()
+                self:parseMessage(msg)
+            end
+        end, function()
+            local timeId = os.startTimer(60)
+            while self.connected == true do
+                local timer = os.pullEvent("timer")
+                if timer[2] == timeId and self.id and self.lobby and self.room then
+                    self:joinRoom(self.lobby, self.room, self.password)
+                    timeId = os.startTimer(60)
+                end
+            end
+        end, eventLoop)
     end,
     captureRecieved = function(self)
 
@@ -58,22 +74,6 @@ local P2P = class({
     on = function(self, eventusername, func)
         self.dispatch[eventusername] = func
     end,
-    help = function(funcusername)
-        local response = ""
-        switch(funcusername, {
-            onReceive = function()
-                response = "accepts function, returns event_username:string,payload:any"
-            end,
-            send = function()
-                response =
-                    "sends event, accepts eventusername:String<required>,payload:string/table<required>,peerId:string<optional>"
-            end,
-            default = function()
-                response = "Returns Function Descriptions"
-            end
-        })
-        return response
-    end,
     getPeers = function(self)
         return self.peers
     end,
@@ -91,28 +91,14 @@ local P2P = class({
         self.peers = response.error and {} or response
 
     end,
-    connect = function(self, config)
-        self.lobby = config.lobby
-        self.room = config.room
-        self.password = config.password
+    connect = function(self, lobby, room, password)
+        self.lobby = lobby
+        self.room = room
+        self.password = password
         self:send("connect", {
             username = self.username
         })
-        parallel.waitForAll(function()
-            while self.connected do
-                local msg = self.ws.receive()
-                self:parseMessage(msg)
-            end
-        end, function()
-            local timeId = os.startTimer(60)
-            while self.connected == true do
-                local timer = os.pullEvent("timer")
-                if timer[2] == timeId and self.id then
-                    self:joinRoom(self.lobby, self.room, self.password)
-                    timeId = os.startTimer(60)
-                end
-            end
-        end, config.eventLoop)
+
     end,
     close = function(self)
         self.connected = false
